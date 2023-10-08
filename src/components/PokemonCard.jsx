141ebd74f2ddf668226d4dd32f9c1492c233576c
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-10-08 23:13:01 +0200

    add pokemonList

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,21 @@
-function PokemonCard() {
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
+  function PokemonCard({pokemon}) {
+    const imageSrc = pokemon.imgSrc? pokemon.imgSrc : '???';
+
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+        <img src={imageSrc}
+        alt={pokemon.name} />
+        <figcaption>{pokemon.name}</figcaption>
     </figure>;
 }
 
