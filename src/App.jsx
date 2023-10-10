5d433fac99e9d39d288ad5fcebc51b6ccc0b9417
VICTOR GUYOMARD <victorguyo44600@gmail.com>
2023-10-10 09:58:00 +0200

    add pokemon props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import "../src/App.css"
+
 import PokemonCard from "./components/PokemonCard";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const pokemonL = pokemonList[0];
 
 function App() {
   return (
     <div>
 
-        <PokemonCard />
+        <PokemonCard pokemon = {pokemonL}/>
         
     </div>
 );
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-  const PokemonCard = () => {
-  const pokemon = pokemonList[1];
+
+
+  const PokemonCard = ({pokemon}) => {
+
+console.log(pokemon)
+
     return (
       <figure>
        {
